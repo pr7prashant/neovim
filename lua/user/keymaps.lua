3c6a4ec36039310494600f9e6b288451986e0982
Prashant <pr7prashant@gmail.com>
2022-09-07 09:44:41 +0530

    update Telescope current_buffer_fuzzy_find keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -57,7 +57,7 @@ keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
 -- Telescope
 keymap("n", "<C-p>", ":Telescope find_files<CR>", opts)
-keymap("n", "<C-f>", ":Telescope current_buffer_fuzzy_find<CR>", opts)
+keymap("n", "<leader>f", ":Telescope current_buffer_fuzzy_find<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
