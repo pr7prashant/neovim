cc9ff316508ee974d0c8ef9dc5a5a6218dcf4044
Prashant <pr7prashant@gmail.com>
2022-09-11 20:31:10 +0530

    add keymap to move text up/down

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -50,6 +50,22 @@ keymap("i", "jk", "<ESC>", opts)
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+-- Move text up and down
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Retain yanked value in register
+keymap("v", "p", '"_dP', opts)
+
 -- Plugins --
 
 -- NvimTree
