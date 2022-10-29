f0a44130521010cbb7a8af6fc461daca69d7ac9c
Prashant <pr7prashant@gmail.com>
2022-10-29 16:40:57 +0530

    silence eslint warnings null ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -18,7 +18,7 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.google_java_format,
-		diagnostics.eslint,
+		diagnostics.eslint.with({ extra_args = { "--quiet" } }),
 		diagnostics.flake8,
 	},
 	-- you can reuse a shared lspconfig on_attach callback here
