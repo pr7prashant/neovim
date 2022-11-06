369206e6bf796f02618d22cc64da7612fc44c878
Prashant <pr7prashant@gmail.com>
2022-11-06 16:01:19 +0530

    add rustfmt null-ls formatter for rust

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -16,6 +16,7 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.google_java_format,
+		formatting.rustfmt,
 		diagnostics.flake8,
 		diagnostics.eslint.with({ extra_args = { "--quiet" } }),
 	},
