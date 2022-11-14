83b19d0fefa56e434019df1084c1b29cb7742514
Prashant <pr7prashant@gmail.com>
2022-11-14 18:51:03 +0530

    removed unnecessary autocommand and replace it with an option

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -38,13 +38,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 })
 
 vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
--- vim.api.nvim_create_autocmd({ "BufEnter" }, {
---   callback = function()
---     vim.cmd [[
---       if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
---     ]]
---   end,
--- })
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
 	callback = function()
@@ -55,13 +48,6 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
 vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
 	callback = function()
 		vim.cmd("quit")
-	end,
-})
-
--- Fixes Autocomment
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	callback = function()
-		vim.cmd("set formatoptions-=cro")
 	end,
 })
 
