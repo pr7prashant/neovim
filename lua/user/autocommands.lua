ffe5e41dc191e47f2dd11a30cae0355ee324807c
Prashant <pr7prashant@gmail.com>
2023-03-08 09:48:22 +0530

    document an unclear autocmd

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -28,6 +28,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
+-- Automatically close tab/vim when nvim-tree is the last window in the tab
 vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
