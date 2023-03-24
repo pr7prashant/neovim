aad85ff62a54a69b8e0becd4c56bfc3bfd5d7159
Prashant <pr7prashant@gmail.com>
2023-03-24 23:48:10 +0530

    enable incremental search

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -41,3 +41,4 @@ vim.opt.whichwrap:append "<,>,[,],h,l" -- keys allowed to move to the previous/n
 vim.opt.iskeyword:append "-" -- treats words with `-` as single words
 vim.opt.formatoptions:remove { "c", "r", "o" } -- This is a sequence of letters which describes how automatic formatting is to be done
 vim.opt.linebreak = true
+vim.opt.incsearch = true -- incremental search
