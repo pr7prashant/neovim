44e196a9bef78e603faa040dc140cd774c083695
Prashant <pr7prashant@gmail.com>
2023-04-16 15:35:06 +0530

    update pattern to detect project root

--- lua/user/project.lua
+++ lua/user/project.lua
@@ -8,7 +8,7 @@ project.setup {
   detection_methods = { "pattern" },
 
   -- patterns used to detect root dir, when **"pattern"** is in detection_methods
-  patterns = { ".git", "Makefile", "package.json" },
+  patterns = { ".git", "Makefile" },
 }
 
 local tele_status_ok, telescope = pcall(require, "telescope")
